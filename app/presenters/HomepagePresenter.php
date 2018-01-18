7615c1d94dce0d6a406d571657f287d50fb9ecdf
Tom Barton <grez.cz@gmail.com>
2018-01-18 15:43:08 +0100

    [Topics] Added creating topics

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -3,8 +3,68 @@
 namespace App\Presenters;
 
 use Nette;
+use Nette\Application\UI\Form;
+use Nette\Database\Context;
+use Nette\Utils\ArrayHash;
+
 
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+
+    /**
+     * @inject
+     * @var Context
+     */
+    public $database;
+
+
+
+    public function renderDefault()
+    {
+        $this->template->topics = $this->database->table('topics')
+            ->order('name')
+            ->fetchAll();
+    }
+
+
+
+    protected function createComponentAddTopicForm()
+    {
+        $form = new Form();
+        $form->addProtection();
+
+        $form->addText('name', 'Téma')
+            ->setRequired();
+
+        $form->addTextArea('description', 'Popisek')
+            ->setRequired()
+            ->addRule($form::MAX_LENGTH, 'Maximální délka popisu je %d znaků', 255);
+
+        $form->addSubmit('send', 'Vytvořit');
+
+        $form->onSubmit[] = function (Form $form, ArrayHash $values) {
+            $topic = $this->database->table('topics')
+                ->where('name', $values->name)
+                ->fetch();
+
+            if ($topic === FALSE) {
+                $this->flashMessage('Téma už existuje', 'danger');
+                $this->redirect('this');
+            }
+        };
+
+        $form->onSuccess[] = function (Form $form, ArrayHash $values) {
+            $topic = $this->database->table('topics')->insert([
+                'name' => $values->name,
+                'description' => $values->description,
+            ]);
+
+            $this->flashMessage('Příspěvek přidán', 'sucess');
+            $this->redirect('Topics:detail', ['id' => $topic->id]);
+        };
+
+        return $form;
+    }
+
 }
---
+++ app/presenters/TopicsPresenter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI\Form;
+use Nette\Database\Context;
+use Nette\Utils\ArrayHash;
+
+
+
+class TopicsPresenter extends Nette\Application\UI\Presenter
+{
+
+
+
+}
