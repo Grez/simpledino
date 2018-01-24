ddda280930bd221c6f88f43b9c435199a3cb0896
Tom Barton <grez.cz@gmail.com>
2018-01-24 15:25:36 +0100

    Gulp config, added bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,31 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var concatCss = require('gulp-concat-css');
+var bower = require('gulp-bower');
 
-gulp.task('default', defaultTask);
+gulp.task('css', function() {
+	var files = [
+		'./www/bower_components/bootstrap/dist/css/bootstrap.min.css',
+	];
 
-function defaultTask(done) {
-  // place code for your default task here
-  done();
-}
+	return gulp.src(files)
+		.pipe(concatCss('style.css'))
+		.pipe(gulp.dest('./www/css'));
+});
+
+gulp.task('js', function () {
+	var files = [
+		'./www/bower_components/bootstrap/dist/js/bootstrap.min.js',
+	];
+
+	return gulp.src(files)
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('./www/js'));
+});
+
+gulp.task('bower', function() {
+	return bower()
+		.pipe(gulp.dest('./www/bower_components'))
+});
+
+gulp.task('default', gulp.series('bower', 'css', 'js'));
