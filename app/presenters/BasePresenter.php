114d2b8c3ac128ad2823b0fcd41912f0fe188640
Tom Barton <grez.cz@gmail.com>
2018-01-29 11:50:10 +0100

    Better API for flash messages

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -12,28 +12,28 @@ use Nette\Utils\ArrayHash;
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
 
-    protected function infoFlashMessage(string $message)// : void
+    protected function infoFlashMessage(string $message): void
     {
         $this->flashMessage($message, 'info');
     }
 
 
 
-    protected function warningFlashMessage(string $message)// : void
+    protected function warningFlashMessage(string $message): void
     {
         $this->flashMessage($message, 'warning');
     }
 
 
 
-    protected function successFlashMessage(string $message)// : void
+    protected function successFlashMessage(string $message): void
     {
         $this->flashMessage($message, 'success');
     }
 
 
 
-    protected function dangerFlashMessage(string $message)// : void
+    protected function dangerFlashMessage(string $message): void
     {
         $this->flashMessage($message, 'danger');
     }
@@ -41,9 +41,9 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
 
 
     /**
-     * @deprecated use $this->dangerFlashMessage()
+     * @deprecated use $this->dangerFlashMessage(), this is just alias
      */
-    protected function errorFlashMessage(string $message)// : void
+    protected function errorFlashMessage(string $message): void
     {
         $this->dangerFlashMessage($message);
     }
